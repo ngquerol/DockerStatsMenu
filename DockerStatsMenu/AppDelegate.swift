76de4e29032f8a5eb5c29e98c7a6e876ce35a3aa
Nicolas G. Querol <nicolas.gquerol@gmail.com>
2016-11-14 03:18:14 +0100

    Initial Commit

---
+++ DockerStatsMenu/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  DockerStatsMenu
+//
+//  Created by Nicolas Gaulard-Querol on 14/11/2016.
+//  Copyright © 2016 Nicolas Gaulard-Querol. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
